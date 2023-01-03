8809cc1af559a3a974865c47c7672fc1e657b169
KarimovAbdullo <6510206@mail.ru>
2023-01-03 15:35:33 +0500

    create rule

---
+++ src/screens/CreateRuleScreen/CreateRuleScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import Container from '../../components/Container'
+ 
+
+export const CreateRuleScreen =()=>{
+  return(
+    <View>
+      <Typo
+    </View>
+  )
+}
